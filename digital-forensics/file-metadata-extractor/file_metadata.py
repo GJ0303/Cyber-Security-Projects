605e86f384b44392b94f699eb8bec8b96cb61a91
GJ0303 <geetj0303@gmail.com>
2026-01-07 16:37:43 +0530

    Implement file metadata extraction function
    Added a function to extract and display file metadata.

--- digital-forensics/file-metadata-extractor/file_metadata.py
+++ digital-forensics/file-metadata-extractor/file_metadata.py
@@ -1,1 +1,13 @@
+import os
+from datetime import datetime
 
+def get_metadata(path):
+    stats = os.stat(path)
+
+    print("File:", path)
+    print("Size:", stats.st_size, "bytes")
+    print("Created:", datetime.fromtimestamp(stats.st_ctime))
+    print("Modified:", datetime.fromtimestamp(stats.st_mtime))
+
+file = input("Enter file path: ")
+get_metadata(file)
